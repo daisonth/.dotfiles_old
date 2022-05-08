a33bff80080edfeec18ea77ec2e9c792a3c7be6b
Daison Thomas <daisonthms@gmail.com>
2022-05-08 18:31:05 +0530

    s

--- .bashrc
+++ .bashrc
@@ -34,6 +34,7 @@ export NNN_PLUG='v:imgview;p:preview-tui;k:kdeconnect;f:fixname;g:fzcd'
 if [ -f /usr/share/nnn/quitcd/quitcd.bash_zsh ]; then
     source /usr/share/nnn/quitcd/quitcd.bash_zsh
 fi
+alias n='n -de'
 
 PS1='[\u@\h \W]\$ '
 
--- scripts/edit-dotfiles.sh
+++ scripts/edit-dotfiles.sh
@@ -4,84 +4,68 @@ str=$'alacritty\nspectrwm\nHerbstluftwm\nnvim\ndmenu\nst\nranger\nzathurarc\n.st
 read p <<< $(echo "$str"|dmenu)
 
 if [ $p == "alacritty" ];then
-  cd $HOME/.dotfiles/.config/alacritty/
-  notify-send "Edit alacritty config file" -t 5000
+  cd /home/daison/.dotfiles/.config/alacritty/
   alacritty -e nvim alacritty.yml
 
 elif [ $p == "spectrwm" ];then
-  cd $HOME/.dotfiles/.config/spectrwm/
-  notify-send "Edit spectrwm config file" -t 5000
+  cd /home/daison/.dotfiles/.config/spectrwm/
   alacritty -e nvim spectrwm.conf
 
 elif [ $p == "Herbstluftwm" ];then
-  cd $HOME/.dotfiles/.config/herbstluftwm/
-  notify-send "Edit Herbstluftwm config file" -t 5000
+  cd /home/daison/.dotfiles/.config/herbstluftwm/
   alacritty -e nvim autostart
 
 elif [ $p == "nvim" ];then
-  cd $HOME/.dotfiles/.config/nvim/
-  notify-send "Edit nvim config file" -t 5000
+  cd /home/daison/.dotfiles/.config/nvim/
   alacritty -e nvim init.lua
 
 elif [ $p == "dmenu" ];then
-  cd $HOME/.dotfiles/dmenu/
-  notify-send "Edit dmenu config file" -t 5000
+  cd /home/daison/.dotfiles/dmenu/
   alacritty -e nvim config.def.h
 
 elif [ $p == "st" ];then
-  cd $HOME/.dotfiles/st/
-  notify-send "Edit alacritty config file" -t 5000
+  cd /home/daison/.dotfiles/st/
   alacritty -e nvim config.def.h 
 
 elif [ $p == "ranger" ];then
-  cd $HOME/.dotfiles/.config/ranger/
-  notify-send "Edit ranger config file" -t 5000
+  cd /home/daison/.dotfiles/.config/ranger/
   alacritty -e nvim rc.conf 
 
 elif [ $p == "zathurarc" ];then
-  cd $HOME/.dotfiles/.config/zathura/
-  notify-send "Edit zathura config file" -t 5000
+  cd /home/daison/.dotfiles/.config/zathura/
   alacritty -e nvim zathurarc
 
 elif [ $p == ".dunstrc" ];then
-  cd $HOME/.dotfiles/.config/dunst/
-  notify-send "Edit dunstrc" -t 5000
+  cd /home/daison/.dotfiles/.config/dunst/
   alacritty -e nvim dunstrc
 
 elif [ $p == ".xinitrc" ];then
-  cd $HOME/.dotfiles/
-  notify-send "Edit .xinitrc" -t 5000
+  cd /home/daison/.dotfiles/
   alacritty -e nvim .xinitrc
 
 elif [ $p == ".bashrc" ];then
-  cd $HOME/.dotfiles/
-  notify-send "Edit .bashrc" -t 5000
+  cd /home/daison/.dotfiles/
   alacritty -e nvim .bashrc
  
 elif [ $p == ".fehbg" ];then
-  cd $HOME/.dotfiles/
-  notify-send "Edit .fehbg" -t 5000
+  cd /home/daison/.dotfiles/
   alacritty -e nvim .fehbg
  
 elif [ $p == ".conkyrc" ];then
-  cd $HOME
-  notify-send "Edit .conkyrc" -t 5000
+  cd /home/daison
   alacritty -e nvim .conkyrc
 
 elif [ $p == ".stow-local-ignore" ];then
-  cd $HOME/.dotfiles/
-  notify-send "Edit .stow-local-ignore" -t 5000
+  cd /home/daison/.dotfiles/
   alacritty -e nvim .stow-local-ignore
   
 elif [ $p == "qutebrowser" ];then
-  cd $HOME/.dotfiles/.config/qutebrowser/
-  notify-send "Edit qutebrowser config file" -t 5000
+  cd /home/daison/.dotfiles/.config/qutebrowser/
   alacritty -e nvim config.py
 
 elif [ $p == "scripts" ];then
-  cd $HOME/.dotfiles/scripts/
+  cd /home/daison/.dotfiles/scripts/
   read p <<< $(ls|dmenu)
-  notify-send "Edit $p" -t 5000
   alacritty -e nvim $p
 
 else 
